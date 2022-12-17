2bf29efe25fd187c56fce3f11e3882c859056e15
Dasharath Dyavari <dyavaridasharath82@gmail.com>
2022-12-17 13:43:20 +0530

    feat(sidebar): design sidebar

--- my_google_maps/src/components/navbar/home/sidebar/sidebar.js
+++ my_google_maps/src/components/navbar/home/sidebar/sidebar.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import PlaceIcon from '@mui/icons-material/Place';
+import SwapVertIcon from '@mui/icons-material/SwapVert';
 
 function Sidebar() {
   return (
-    <div className='sidebar'>sidebar</div>
+    <div className='sidebar'>
+        <div className="locations_container">
+            <div className="location">
+                <PlaceIcon className='location-icon'/>
+                <div  className='location-search'>
+                    <input type="text" placeholder='Choose starting point'/>
+                </div>
+            </div>
+            <div className="location">
+                <PlaceIcon className='location-icon'/>
+                <div className='location-search'>
+                    <input type="text" placeholder='Choose destination'/>
+                </div>
+            </div>
+        </div>
+        <button className='swap-button'><SwapVertIcon className='swap-icon'/></button>
+    </div>
   )
 }
 
